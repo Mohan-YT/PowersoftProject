ab495c03e075f1c16a7cd6b07a70bc00eee4f065
Mohan <mohanprasanth3883@gmail.com>
2025-04-27 15:07:20 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
 import cors from 'cors'
 import cloudinary from 'cloudinary'
+import path from 'path'
 
 import MongoDB from "./Database/MongoDB.js";
 import empRoute from "./Routes/EmpRoute.js";
@@ -11,6 +12,7 @@ import projectRouter from "./Routes/ProjectRoute.js";
 import taskRouter from './Routes/TaskRouter.js'
 
 dotenv.config()
+
 cloudinary.config({
     cloud_name : process.env.CLOUNDINARY_NAME ,
     api_key : process.env.CLOUNDINARY_API_KEY ,
@@ -20,6 +22,7 @@ cloudinary.config({
 
 const PORT = process.env.PORT;
 const app = express()
+const __dirname = path.resolve()
 
 app.use(cors({
     origin : 'http://localhost:5173',
@@ -36,6 +39,14 @@ app.use(express.urlencoded({
 app.use('/employees',empRoute)
 app.use('/projects',projectRouter)
 app.use('/tasks',taskRouter)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
+      app.get("/*splat", (req, res) => {
+          res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+      });
+  }
 
 app.listen(PORT,()=>{
     console.log(`Server run port number ${PORT}`)
